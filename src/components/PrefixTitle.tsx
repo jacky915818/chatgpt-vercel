9d1fa42082436b5e4503b0de10463b120a68ce48
jacky915818 <67189816+jacky915818@users.noreply.github.com>
2023-10-26 00:21:34 +0800

    Update PrefixTitle.tsx

--- src/components/PrefixTitle.tsx
+++ src/components/PrefixTitle.tsx
@@ -2,5 +2,5 @@ import type { JSXElement } from "solid-js"
 import { Title } from "solid-start"
 
 export default function (props: { children?: JSXElement }) {
-  return <Title>一宝Ai{props.children ? " | " + props.children : ""}</Title>
+  return <Title>ChatGPT{props.children ? " | " + props.children : ""}</Title>
 }
